a129d569be654b758ff71684797f47354326c658
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-05 00:11:15 +0700

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  function firstNameHandle(event) {
+    setFirstName(event.target.value);
+    setFullName(firstName + lastName);
+  }
+  function lastNameHandle(event) {
+    setLastName(event.target.value);
+    setFullName(firstName + lastName);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={firstNameHandle}
+          name="fName"
+          placeholder="First Name"
+          // value={firstName}
+        />
+        <input
+          onChange={lastNameHandle}
+          name="lName"
+          placeholder="Last Name"
+          // value={lastName}
+        />
         <button>Submit</button>
       </form>
     </div>
