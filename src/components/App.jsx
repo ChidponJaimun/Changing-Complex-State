2efdef13266fe4c63b862612ff314c2e5dec7a78
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-05 00:35:41 +0700

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,19 @@ import React, { useState } from "react";
 function App() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [fullName, setFullName] = useState("");
 
   function firstNameHandle(event) {
     setFirstName(event.target.value);
-    setFullName(firstName + lastName);
   }
   function lastNameHandle(event) {
     setLastName(event.target.value);
-    setFullName(firstName + lastName);
   }
 
   return (
     <div className="container">
-      <h1>Hello {fullName}</h1>
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
       <form>
         <input
           onChange={firstNameHandle}
