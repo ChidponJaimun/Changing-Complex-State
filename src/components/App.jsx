1caef84e29874cb9114f5c77bcb29513440e79b5
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-05 02:44:03 +0700

    0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,10 @@ function App() {
     const newValue = event.target.value;
     const inputName = event.target.name;
 
-    if (inputName === "fname") {
-      setFullName({ firstName: newValue });
-    } else {
-      setFullName({ lastName: newValue });
+    if (inputName === "fName") {
+      setFullName({ firstName: newValue, lastName: fullName.lastName });
+    } else if (inputName === "lName") {
+      setFullName({ firstName: fullName.firstName, lastName: newValue });
     }
   }
 
@@ -27,13 +27,13 @@ function App() {
           onChange={updateName}
           name="fName"
           placeholder="First Name"
-          // value={fullName.firstName}
+          value={fullName.firstName}
         />
         <input
           onChange={updateName}
           name="lName"
           placeholder="Last Name"
-          // value={fullName.lastName}
+          value={fullName.lastName}
         />
         <button>Submit</button>
       </form>
